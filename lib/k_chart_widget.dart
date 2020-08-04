490b5a7337e61b7bdcecc420812a2c2baf6c7152
flh_wangyahui <wangyahui0119@dingtalk.com>
2020-08-04 09:53:10 +0800

    k线弹窗英文

--- lib/k_chart_widget.dart
+++ lib/k_chart_widget.dart
@@ -143,7 +143,7 @@ class _KChartWidgetState extends State<KChartWidget>  with SingleTickerProviderS
 
   void notifyChanged() => setState(() {});
 
-  List<String> infoNames = ["时间", "开", "高", "低", "收", "涨跌额", "涨幅", "成交量"];
+  List<String> infoNames = ["Time", "Open", "High", "Low", "Close", "UpDown", "Change", "Vol"];
   List infos;
 
   Widget _buildInfoDialog() {
